74d59d01fddfa96ffdfee4255adc47261f17a09e
jruizrojas1406 <jruizrojas1406@gmail.com>
2023-03-01 11:15:32 -0500

    jQuery-validate homologado para bootstrap v.4+

--- Ingemmet.FirmaDocumento/Ingemmet.FirmaDocumento.Web/Scripts/jquery-validate/jquery.validate.setextends.js
+++ Ingemmet.FirmaDocumento/Ingemmet.FirmaDocumento.Web/Scripts/jquery-validate/jquery.validate.setextends.js
@@ -1,9 +1,10 @@
 ﻿$.validator.setDefaults({
+    ignore: '*:not([name])',
     errorElement: "span",
-    errorClass: "help-block",
+    errorClass: "help-block invalid-feedback",
     errorPlacement: function (error, element) {
         if (element.attr("type") === "radio" || element.attr("type") === "checkbox") {
-            error.insertAfter($(element).closest('.form-group').children('div').children().last());
+            error.appendTo($(element).closest('.form-group'));
         } else if (element.hasClass("ckeditor")) {
             error.appendTo($(element).closest('.form-group'));
         } if (element.hasClass("select2") || element.hasClass('select2-hidden-accessible')) {
@@ -16,31 +17,17 @@
             else
                 error.insertAfter(element);
         }
+        $(element).closest('.form-group').addClass('has-error');
     },
-    ignore: '*:not([name])',
-    //invalidHandler: function (event, validator) { 
-    //    successHandler2.hide();
-    //    errorHandler2.show();
-    //},
-    highlight: function (element) {
-        $(element).closest('.help-block').removeClass('valid');
-        $(element).closest('.form-group').removeClass('has-success').addClass('has-error').find('.symbol').removeClass('ok').addClass('required');
+    highlight: function (element, errorClass, validClass) {
+        $(element).addClass('is-invalid').removeClass('is-valid');
+        $(element).closest('.form-group').addClass('has-error');
     },
-    unhighlight: function (element) {
+    unhighlight: function (element, errorClass, validClass) {
+        $(element).removeClass('is-invalid').addClass('is-valid');
         $(element).closest('.form-group').removeClass('has-error');
     },
     success: function (label, element) {
-        label.addClass('help-block valid');
-        $(element).closest('.form-group').removeClass('has-error').addClass('has-success').find('.symbol').removeClass('required').addClass('ok');
-    },
-    //highlight: function (element, errorClass, validClass) {
-    //    $(element).addClass('is-invalid');
-    //},
-    //unhighlight: function (element, errorClass, validClass) {
-    //    $(element).removeClass('is-invalid');
-    //},
-    //errorPlacement: function (error, element) {
-    //    error.addClass('invalid-feedback');
-    //    element.closest('.form-group').append(error);
-    //}
+        $(element).removeClass('is-invalid').addClass('has-error');
+    }
 });
